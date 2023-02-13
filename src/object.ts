4fa134bab7afa2096968cab954c106d525092704
benzo-y <fukubenzo@gmail.com>
2023-02-13 11:46:37 +0900

    add object.ts

---
+++ src/object.ts
@@ -0,0 +1,10 @@
+export {};
+
+let profile: object = { name: 'Ham'};
+profile = { brithYear: 1976 };
+
+let profile2: {
+  name: string;
+} = { name: 'Ham'};
+// profile2 = { brithYear: 1976 };
+profile2 = { name: 'Nimo' };
